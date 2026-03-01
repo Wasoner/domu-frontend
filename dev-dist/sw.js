a91fec131d319b719189c351f1e28b1f120b8750
Wasoner <cristobal.rivas2001@alumnos.ubiobio.cl>
2026-02-28 23:27:49 -0300

    fix: actualizar la revisión del archivo index.html en el service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b6f6htg4hho"
+    "revision": "0.jol9svn5kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
